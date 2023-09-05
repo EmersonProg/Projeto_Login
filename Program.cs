8f3c2f6a91bcfacdb5a16aa2e45001bba789d667
EmersonProg <95104916+EmersonProg@users.noreply.github.com>
2023-09-05 15:46:53 -0300

    Atualizado para Iniciarmos o projeto
    Bons Estudos

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Projeto para criação de login
+/********************Vitor**************
+Voce irá criar duas variáveis:
+1º - Usuário e vai guardar nela Admin
+2º - Senha e vai guardar nela 123456
+
+*********************Xande**************************
+Você irá criar duas variáveis:
+1º - tentativa para guardar o número máximo de tentativas e vai guardar nela o némero 3
+2º - acesso e vai guardar dentro dela false (essa variavel vai ser para controlarmos o acesso e ela é do tipo booleano)
+
+**************************Renan**************************************
+Você vai fazer o código para contar a quantidade de tentativas sendo o máximo 3:
+1º - Pedir para o usuário digitar  usuário e depois a senha
+o código tem que contar e monstrando a cada tentativa caso seja usuaria e senha sejam invalidos Ex:
+Tentativa 1/3
+Usuário ou senha iválidos
+Tentativa 2/3
+Usuário ou senha iválidos
+Tentativa 3/3
+Usuário ou senha iválidos
+depois da terceira parar o loop e mostrar usuário e senha invalidos
+2º - E se usuario digitar correto mostrar usuario logado
+
+*************************************************************************************
+Após finalizarmos esse código vamos entende-lo e melhorar ele para praticarmos mais ok!
